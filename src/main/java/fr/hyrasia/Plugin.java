7e94543bdd45d63fcde62a5587815d7c405bdbfc
DracoLeNoob <unconfigured@null.spigotmc.org>
2023-01-13 15:19:33 +0100

    Premier commit : Création du plugin

---
+++ src/main/java/fr/hyrasia/Plugin.java
@@ -0,0 +1,10 @@
+package fr.hyrasia;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Plugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        System.out.println("Everything is working perfectly");
+    }
+}
